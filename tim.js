f3a74ab23e173c1e6b31cdf120eca1364da5e004
gouravkumarbhagat <gouravkumarbhagat474@gmail.com>
2026-04-07 17:39:09 +0530

    finally

--- tim.js
+++ tim.js
@@ -1,3 +1,4 @@
+
 const events = document.querySelectorAll(".event");
 const timeline = document.querySelector(".timeline");
 
@@ -14,8 +15,59 @@ window.addEventListener("scroll", () => {
     }
   });
 
-  // Optional: grow the line based on how many events are visible
   const total = events.length;
   const percent = (visibleCount / total) * 100;
   timeline.style.setProperty("--line-height", percent + "%");
 });
+
+let music, btn;
+
+window.onload = () => {
+  music = document.getElementById("music");
+  btn = document.getElementById("sound");
+
+  // start icon (OFF)
+  btn.style.backgroundImage = "url('no_sound.png')";
+  btn.style.backgroundSize = "cover";
+  btn.style.backgroundPosition = "center";
+};
+
+function toggleSound() {
+  if (music.paused) {
+    music.play();
+    btn.style.backgroundImage = "url('volume_up.png')"; // ON icon
+  } else {
+    music.pause();
+    btn.style.backgroundImage = "url('no_sound.png')"; // OFF icon
+  }
+}
+
+function lastclick() {
+  window.location.href = "last.html";
+}
+
+// flower code
+const container = document.querySelector(".flowers");
+
+function createFlower() {
+  const flower = document.createElement("div");
+  flower.classList.add("flower");
+
+  flower.innerHTML = "🌸";
+
+  flower.style.left = Math.random() * 100 + "vw";
+
+  const size = Math.random() * 20 + 20;
+  flower.style.fontSize = size + "px";
+
+  const duration = Math.random() * 5 + 5;
+  flower.style.animationDuration = duration + "s";
+
+  container.appendChild(flower);
+
+  setTimeout(() => {
+    flower.remove();
+  }, duration * 1000);
+}
+
+setInterval(createFlower, 300);
